f592daa66778fcd0469a6c3882f3fce1d8fc44f4
kmendess <karolmendes.rj@gmail.com>
2022-05-19 23:25:22 -0300

    Added ProjectsController

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,57 @@
+using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // if invalid object
+            // return NotFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            // if invalid object
+            // return BadRequest();
+
+            // Create object
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectModel updateProject)
+        {
+            // if invalid object
+            // return BadRequest();
+
+            // Update object
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // if object not found
+            // return NotFound();
+
+            // Delete object
+
+            return NoContent();
+        }
+    }
+}
